4f9d9a8980e15f2deb8673dc10697460d99a376e
thejuniordevops <edigithub@gmail.com>
2019-02-21 19:17:11 +0700

    redux boilerplate

---
+++ src/reducers/index.js
@@ -0,0 +1,8 @@
+import { combineReducers } from 'redux';
+
+export default combineReducers({
+
+    libraries: () => { // create a reducer named libraries
+        return [];     // return an array
+    } 
+});
